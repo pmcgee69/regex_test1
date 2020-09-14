ca5f6a15217e39d2a62a31d03550d38fd178e8d2
Paul McGee <paulmcgee1969@gmail.com>
2020-09-15 04:26:44 +0800

    Upload files.

---
+++ Project1.dpr
@@ -0,0 +1,44 @@
+program Project1;
+{$APPTYPE CONSOLE}
+uses
+  System.SysUtils, System.RegularExpressions, classes;
+const
+  regex_pattern   = '"(Keyword|Keyuword2).+?,';
+var
+  Regex           : TRegex;
+  MatchCollection : TMatchCollection;
+  x               : integer;
+  sl              : TStringlist;
+  line            : string;
+begin
+   sl := Tstringlist.Create;
+
+   writeln;
+   sl.loadfromfile('..\..\reg_test_1.txt');
+   for line in sl do  begin
+      //writeln(line);
+      Regex := TRegex.create(regex_pattern);
+      MatchCollection := Regex.Matches(line);
+      for x := 0 to MatchCollection.Count-1 do begin
+          writeln(x.tostring, '  ', MatchCollection.item[x].value);
+          writeln;
+      end;
+   end;
+
+   writeln;
+   sl.loadfromfile('..\..\reg_test_2.txt');
+   for line in sl do  begin
+      //writeln(line);
+      Regex := TRegex.create(regex_pattern);
+      MatchCollection := Regex.Matches(line);
+      for x := 0 to MatchCollection.Count-1 do begin
+          writeln(x.tostring, '  ', MatchCollection.item[x].value);
+          writeln;
+      end;
+   end;
+
+   sl.free;
+   readln;
+end.
+
+
